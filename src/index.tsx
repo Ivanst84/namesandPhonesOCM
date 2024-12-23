605c2f0bfb5607645fa17f7c63c1c81b8268197a
ivanst84 <ivan.sosatovar@gmail.com>
2024-12-23 13:39:27 -0800

    estado mantenimiento

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom/client';
 import '../src/index.css';
 import App from './App';
 
+const isMaintenance = true; // Cambia a false para mostrar tu app
+
+const Maintenance = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>Se esta trabajando para mejorar el sitio</h1>
+      <p>Estamos trabajando en ello. Por favor,contacta al administrador del sitio .</p>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    {isMaintenance ? <Maintenance /> : <App />}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
